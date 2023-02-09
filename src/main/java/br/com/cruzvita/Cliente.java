e6ca66ab29daaee1a89398d3ec39686495cf9811
LeandroCg99 <leandro.cgeronimo@hotmail.com>
2023-02-09 17:53:26 -0300

    Atualizacao classe teste

--- src/main/java/br/com/cruzvita/Cliente.java
+++ src/main/java/br/com/cruzvita/Cliente.java
@@ -5,7 +5,7 @@ public class Cliente {
 	public String nome;
 	public Integer idade;
 //public = modificador de acesso 
-	//String, Integer, Boolean = tipo de retorno
+	//String, Integer, Boolean, = tipo de retorno
 	
 	public String cliente() {
 		return "classe cliente";
--- src/main/java/br/com/cruzvita/teste.java
+++ src/main/java/br/com/cruzvita/teste.java
@@ -6,14 +6,34 @@ public class teste {
 	public static void main(String[] args) {
 		Scanner teclado = new Scanner(System.in);
 
-		Banco a = new Banco();
-		System.out.println(a.imprimir());
+		Banco banco = new Banco();
+		// System.out.println(a.imprimir());
 
-		Cliente b = new Cliente();
-		System.out.println(b.cliente());
+		Cliente cliente = new Cliente();
+		// System.out.println(b.cliente());
 
-		Conta c = new Conta();
-		System.out.println(c.Conta());
+		Conta conta = new Conta();
+		// System.out.println(c.Conta());
 
-	}	
-}
+		System.out.println("Digite o nome do Banco:");
+		banco.nomeBanco = teclado.next();
+		System.out.println("O banco inserido foi: " + banco.nomeBanco);
+
+		teclado.nextLine();
+
+		System.out.println("Digite a modalidade da conta, sendo Conta Poupanca ou Conta Corrente");
+		conta.tipoDeConta = teclado.nextLine();
+		while (!conta.tipoDeConta.equalsIgnoreCase("Conta Poupanca") && !conta.tipoDeConta.equalsIgnoreCase("Conta Corrente")) {
+			System.out.println("Modalidade inválida. Por favor, digite novamente:");
+			conta.tipoDeConta = teclado.nextLine();
+		}
+
+		System.out.println("Escreva seu nome:");
+		cliente.nome = teclado.nextLine();
+
+		System.out.println("Escreva sua idade:");
+		cliente.idade = teclado.nextInt();
+		System.out.println("O nome inserido foi " + cliente.nome + ", onde o mesmo possui " + cliente.idade + " anos.");
+	}
+ }
+
